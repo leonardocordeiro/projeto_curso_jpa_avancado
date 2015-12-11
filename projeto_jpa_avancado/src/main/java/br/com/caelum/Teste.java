b94e0161bb1179eb21ef011377dcb55d4ea72d3a
Leonardo Cordeiro <leonardo.cordeiro@caelum.com.br>
2015-12-11 02:52:56 -0200

    mudando classe teste

--- projeto_jpa_avancado/src/main/java/br/com/caelum/Teste.java
+++ projeto_jpa_avancado/src/main/java/br/com/caelum/Teste.java
@@ -8,17 +8,17 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 public class Teste {
 	public static void main(String[] args) throws SQLException, PropertyVetoException {
 		ComboPooledDataSource dataSource = new JpaConfigurator().getDataSource();
-
-		for(int i = 0; i < 10; i++) {
+		
+		for(int i = 0; i < 10; i ++) {
 			dataSource.getConnection();
 			
-			System.out.println(i + " - Conexões existentes: " + dataSource.getNumConnections());
-			System.out.println(i + " - Conexões ocupadas: " + dataSource.getNumBusyConnections());
-			System.out.println(i + " - Conexões stand-by: " + dataSource.getNumIdleConnections());
+			System.out.println(i + 1 + " - Conexões existentes: " + dataSource.getNumConnections());
+			System.out.println(i + 1 + " - Conexões ocupadas: " + dataSource.getNumBusyConnections());
+			System.out.println(i + 1 + " - Conexões ociosas: " + dataSource.getNumIdleConnections());
 			
 			System.out.println("");
 		}
-		
+
 		
 	}
 }
